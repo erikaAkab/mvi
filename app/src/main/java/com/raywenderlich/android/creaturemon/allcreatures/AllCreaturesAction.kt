79baa7189dc913c39a3319674ec69550db3b82c7
Erika Akabane <erika.akabane@grupopan.com>
2019-06-12 17:03:14 -0300

    finish intention to action

---
+++ app/src/main/java/com/raywenderlich/android/creaturemon/allcreatures/AllCreaturesAction.kt
@@ -0,0 +1,8 @@
+package com.raywenderlich.android.creaturemon.allcreatures
+
+import com.raywenderlich.android.creaturemon.mvibase.MviAction
+
+sealed class AllCreaturesAction : MviAction {
+    object LoadAllCreaturesAction : AllCreaturesAction()
+    object ClearAllCreaturesAction : AllCreaturesAction()
+}
